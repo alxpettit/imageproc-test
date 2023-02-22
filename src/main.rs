577b80b57642ad636aa6e7771a6a8be91f1cce13
Alexandria <alxpettit@gmail.com>
2023-02-21 23:52:53 -0700

    why is it monochrome?

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod window;
 use window::display_image;
 
 use bevy::prelude::*;
-use bevy::render::camera::RenderTarget::Image;
 use bevy::render::render_resource::Texture;
+use bevy::render::texture::Image;
 
 // #[derive(Resource)]
 // struct ImageData {
 //     image: image::RgbaImage,
 // }
+
+#[derive(Resource)]
+struct Display(Handle<Image>);
 
 fn main() {
     // let image_path = "wrench.png".to_owned();
@@ -23,17 +26,31 @@ fn main() {
         // .insert_resource(ImageData { image })
         .add_startup_system(setup)
         //.add_system(draw_image_system)
+        .add_system(edit_image)
         .run();
 
     //display_image("", &image, 500, 500);
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let handle_image = asset_server.load("wrench.png");
+    commands.insert_resource(Display(handle_image.clone()));
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("wrench.png"),
+        texture: handle_image,
         ..default()
     });
+}
+
+fn edit_image(handle_image: Res<Display>, mut image_assets: ResMut<Assets<Image>>) {
+    if let Some(mut image) = image_assets.get_mut(&handle_image.0) {
+        // Access image data and modify it
+        let pixels = &mut image.data;
+        for (i, pixel) in pixels.iter_mut().enumerate() {
+            // Modify pixel values as needed
+            *pixel = (i as f32 * (1. / 500.)) as u8;
+        }
+    }
 }
 
 // fn draw_image_system(
